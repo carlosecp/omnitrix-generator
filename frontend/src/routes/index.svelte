72f99c6029d49d82e005305746b7275c53922cda
Carlos Castillo <47466248+carlosecp@users.noreply.github.com>
2022-03-17 18:00:32 -0300

    feat: starting to develop the frontend

---
+++ frontend/src/routes/index.svelte
@@ -0,0 +1,21 @@
+<script context="module">
+	export const load = async ({ fetch }) => {
+		const res = await fetch("http://127.0.0.0:8080");
+		const aliens = await res.json();
+
+		return {
+			props: { aliens }
+		}
+	}
+</script>
+
+<script>
+	export let aliens;
+</script>
+
+<div class="grid">
+	{#each aliens as alien}
+		<div>{alien.Name}</div>
+		<img src={alien.ImgURL} />
+	{/each}
+</div>
